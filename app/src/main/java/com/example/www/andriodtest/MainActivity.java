41be20f10ef6a86749b760affb223fdff79000b0
ztz <root>
2016-08-09 15:43:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/www/andriodtest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.www.andriodtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int a=Integer.parseInt("15");
+
+    }
+}
